b05813873d04c65279da12736c3ee9544c01872d
Noopur Agarwal <nupur24056@gmail.com>
2023-03-27 16:16:09 +0530

    Create tabs for json and graph content

--- app/javascript/components/automated-workflows/automated-workflows-dummy-data.js
+++ app/javascript/components/automated-workflows/automated-workflows-dummy-data.js
@@ -51,13 +51,109 @@ export const automatedWorkflowsList = () => {
 
 /** Dummy data for Summary Page */
 
-export const automatedWorkflowsSummary = (recordId) => (
-  [
-    { label: __('Name'), value: `workflow-name-${recordId}` },
-    { label: __('Description'), value: 'category description' },
-    { label: __('Single Value'), value: 'single value' },
-    { label: __('Capture C & U Data'), value: 'false' },
-    { label: __('Single Value'), value: 'false' },
-    { label: __('Default'), value: 'false' },
-  ]
+const summary = (recordId) => ([
+  { label: __('Name'), value: `workflow-name-${recordId}` },
+  { label: __('Description'), value: 'category description' },
+  { label: __('Single Value'), value: 'single value' },
+  { label: __('Capture C & U Data'), value: 'false' },
+  { label: __('Single Value'), value: 'false' },
+  { label: __('Default'), value: 'false' },
+]);
+
+export const jsonData = `        {
+  "Comment": "An example of the Amazon States Language using a choice state.",
+  "StartAt": "FirstState",
+  "States": {
+    "FirstState": {
+      "Type": "Task",
+      "Resource": "docker://agrare/hello-world:latest",
+      "Credentials": {
+        "mysecret": "dont tell anyone"
+      },
+      "Retry": [
+        {
+          "ErrorEquals": [ "States.Timeout" ],
+          "IntervalSeconds": 3,
+          "MaxAttempts": 2,
+          "BackoffRate": 1.5
+        }
+      ],
+      "Catch": [
+        {
+          "ErrorEquals": [ "States.ALL" ],
+          "Next": "FailState"
+        }
+      ],
+      "Next": "ChoiceState"
+    },
+
+    "ChoiceState": {
+      "Type" : "Choice",
+      "Choices": [
+        {
+          "Variable": "$.foo",
+          "NumericEquals": 1,
+          "Next": "FirstMatchState"
+        },
+        {
+          "Variable": "$.foo",
+          "NumericEquals": 2,
+          "Next": "SecondMatchState"
+        },
+        {
+          "Variable": "$.foo",
+          "NumericEquals": 3,
+          "Next": "SuccessState"
+        }
+      ],
+      "Default": "FailState"
+    },
+
+    "FirstMatchState": {
+      "Type" : "Task",
+      "Resource": "docker://agrare/hello-world:latest",
+      "Next": "PassState"
+    },
+
+    "SecondMatchState": {
+      "Type" : "Task",
+      "Resource": "docker://agrare/hello-world:latest",
+      "Next": "NextState"
+    },
+
+    "PassState": {
+      "Type": "Pass",
+      "Result": {
+        "foo": "bar",
+        "bar": "baz"
+      },
+      "ResultPath": "$.result",
+      "Next": "NextState"
+    },
+
+    "FailState": {
+      "Type": "Fail",
+      "Error": "FailStateError",
+      "Cause": "No Matches!"
+    },
+
+    "SuccessState": {
+      "Type": "Succeed"
+    },
+
+    "NextState": {
+      "Type": "Task",
+      "Resource": "docker://agrare/hello-world:latest",
+      "Secrets": ["vmdb:aaa-bbb-ccc"],
+      "End": true
+    }
+  }
+}
+`;
+
+export const automatedWorkflowData = (recordId) => (
+  {
+    summary: summary(recordId),
+    jsonData,
+  }
 );
